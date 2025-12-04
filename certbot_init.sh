4ceece402d595f597957659270e1521694fca77f
root <root@vps7.rockxi.ru>
2025-12-04 08:42:16 +0000

    fix bugs on vps7

--- certbot_init.sh
+++ certbot_init.sh
@@ -1,1 +1,1 @@
-docker-compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot --email stroganovf.t@gmail.com -d kursovaya.rockxi.ru --agree-tos --no-eff-email --force-renewal
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot --email stroganovf.t@gmail.com -d kurs.rockxi.ru --agree-tos --no-eff-email --force-renewal
